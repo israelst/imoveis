6fda457a4e0be86b244074700d19539e62c17ed6
Israel Teixeira <israelst@gmail.com>
2018-03-31 12:21:49 -0300

    Add thumbnail to admin list

--- cms/admin.py
+++ cms/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from imagekit.admin import AdminThumbnail
+from imagekit import ImageSpec
+from imagekit.processors import ResizeToFill
+from imagekit.cachefiles import ImageCacheFile
+
+from cms.models import Place
+
+
+class AdminThumbnailSpec(ImageSpec):
+    processors = [ResizeToFill(150, 150)]
+
+
+def cached_admin_thumb(instance):
+    cached = ImageCacheFile(AdminThumbnailSpec(instance.picture))
+    cached.generate()
+    return cached
+
+
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = ('details', 'thumbnail',)
+    thumbnail = AdminThumbnail(image_field=cached_admin_thumb)
+
+
+admin.site.register(Place, PlaceAdmin)

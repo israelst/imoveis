49799966c17f45c05508d19ddb5ff5b1ef0e274c
Israel Teixeira <israelst@gmail.com>
2018-04-01 11:13:46 -0300

    Readonly admin

--- cms/admin.py
+++ cms/admin.py
@@ -19,8 +19,22 @@ def cached_admin_thumb(instance):
 
 
 class PlaceAdmin(admin.ModelAdmin):
-    list_display = ('details', 'thumbnail',)
     thumbnail = AdminThumbnail(image_field=cached_admin_thumb)
+    list_display = ('details', 'thumbnail',)
+    readonly_fields = ('thumbnail', 'picture', 'details', 'size', 'price')
+    actions = None
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def changeform_view(self, request, object_id=None, form_url='', extra_context=None):
+        extra_context = extra_context or {}
+        extra_context['show_save_and_continue'] = False
+        extra_context['show_save'] = False
+        return super(PlaceAdmin, self).changeform_view(request, object_id, extra_context=extra_context)
 
 
 admin.site.register(Place, PlaceAdmin)

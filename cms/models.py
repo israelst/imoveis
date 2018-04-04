4a21b6db7b7b8a3ba599483668a096e5ba9d6967
Israel Teixeira <israelst@gmail.com>
2018-04-03 23:46:52 -0300

    Add location

--- cms/models.py
+++ cms/models.py
@@ -1,5 +1,9 @@
-from django.db import models
+from django.contrib.gis.db import models
+from django.contrib.gis.geos import Point
 from django.urls import reverse
+
+
+RJ = Point(-22.9013763,-43.1783903)
 
 
 class Place(models.Model):
@@ -7,6 +11,7 @@ class Place(models.Model):
     details = models.TextField()
     size = models.DecimalField(max_digits=9, decimal_places=2)
     price = models.DecimalField(max_digits=15, decimal_places=2)
+    location = models.PointField(default=RJ)
 
     def get_absolute_url(self):
         return reverse('cms:place_detail', args=[str(self.id)])
